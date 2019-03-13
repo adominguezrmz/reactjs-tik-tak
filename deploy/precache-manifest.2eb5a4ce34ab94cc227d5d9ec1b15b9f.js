a9c5110a0accf753851f0a57ede694cda7e9e4a0
Adan Ramirez <adandominguezramirez@gmail.com>
2019-03-13 13:06:26 -0700

    test public site

---
+++ deploy/precache-manifest.2eb5a4ce34ab94cc227d5d9ec1b15b9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c33f1a81ef90162b7d63",
+    "url": "/static/js/main.4d9703d1.chunk.js"
+  },
+  {
+    "revision": "44da22f52b005261d0ba",
+    "url": "/static/js/2.83a43218.chunk.js"
+  },
+  {
+    "revision": "c33f1a81ef90162b7d63",
+    "url": "/static/css/main.16b73749.chunk.css"
+  },
+  {
+    "revision": "25115a18070b7da7e1536e819605e409",
+    "url": "/index.html"
+  }
+];
