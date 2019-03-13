7c9df2ac3e327ce17dfc8a2d606bc1cb775cd660
Adan Ramirez <adandominguezramirez@gmail.com>
2019-03-13 15:52:55 -0700

    Adding sorting moves support

--- src/history.js
+++ src/history.js
@@ -1,20 +1,53 @@
 import React from "react";
 
-export default function History(props) {
-    const moves = props.history.map((step, move) => {
-        const classNames = props.stepNumber === move ? "history-current-step" : "history-step";
-        const desc = move ?
-            'Go to move #' + move :
-            'Go to game start';
+class History extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            ascSortOrder: true
+        }
+        this.handleToggleClick.bind(this);
+    }
+
+    handleToggleClick() {
+        this.setState({
+            ascSortOrder: !this.state.ascSortOrder
+        })
+    }
+
+    render() {
+        const ascSortOrder = this.state.ascSortOrder
+        const toggleText = ascSortOrder
+            ? "Moves Descending"
+            : "Moves Ascending";
+        const movesHist = this.props.history.slice()
+        const len = movesHist.length - 1
+        if (!ascSortOrder)
+            movesHist.reverse()
+        const moves = movesHist.map((step, move) => {
+            let stepNumber = move
+            let isStart = move === 0
+            if (!ascSortOrder) {
+                isStart = move === len
+                stepNumber = (len - move)
+            }
+            const classNames = this.props.stepNumber === stepNumber ? "history-current-step" : "history-step";
+            const desc = (isStart) ? 'Go to game start'
+                : 'Go to move #' + (stepNumber);
+            return (
+                <li key={move} className={classNames}>
+                    <button onClick={() => this.props.onClick(move)}>
+                        {desc}
+                    </button>
+                </li>
+            );
+        });
         return (
-            <li key={move} className={classNames}>
-                <button onClick={() => props.onClick(move)}>
-                    {desc}
-                </button>
-            </li>
+            <div className="history-moves">
+                <button onClick={() => this.handleToggleClick()}>{toggleText}</button>
+                <ol>{moves}</ol>
+            </div>
         );
-    });
-    return (
-        <ol>{moves}</ol>
-    );
+    }
 }
+export default History;
